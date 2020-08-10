90b77e1c073703dbca86c26c58537a3aa706c167
shekharoo <shekuemail@gmail.com>
2020-08-11 00:10:15 +0530

    22

--- Demo/src/githubDemo/FirstClass.java
+++ Demo/src/githubDemo/FirstClass.java
@@ -3,11 +3,6 @@ package githubDemo;
 public class FirstClass {
 	public void write()
 	{
-	    System.out.println("Hi");
-		System.out.println("Hii");
-		System.out.println("Hiih");
-		System.out.println("Bye");
-		System.out.println("Bye");
-		System.out.println("Bye2");
+	  System.out.println("Hello");
 	}
 }
