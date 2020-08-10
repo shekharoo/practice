fa6d144b7bb4995b5495a1e14c1af110020bbc11
shekharoo <shekuemail@gmail.com>
2020-08-10 23:59:22 +0530

    demo conflict

--- Demo/src/githubDemo/FirstClass.java
+++ Demo/src/githubDemo/FirstClass.java
@@ -3,8 +3,9 @@ package githubDemo;
 public class FirstClass {
 	public void write()
 	{
-	
-	System.out.println("Hi");
-	System.out.println("Bye");
+	    System.out.println("Hi");
+		System.out.println("Hii");
+		System.out.println("Hiih");
+		System.out.println("Bye");
 	}
 }
